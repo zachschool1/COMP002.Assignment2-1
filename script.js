06f158047114dd608623228ded0b5f3171cd6192
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-10-24 21:50:18 -0700

    add starter files

---
+++ script.js
@@ -0,0 +1,32 @@
+// For each of the 10 tasks below, write a console.log(...) statement that satisfies the
+// requirement. Write your code to accomplish the programming task beneath each comment.
+
+// 1. Write code to output the following string to the console:
+// "Hello World!"
+
+// 2. Write code to concatenate three strings and output the result to the console:
+// "Hello", " ", "World!"
+
+// 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
+// *Note: You should only have one console.log() statement - how do you add a new line in
+// a string?...
+
+// 4. Write code to output the result of the arithmetic expression to the console:
+// 3 * 8 + 2;
+
+// 5. Write code to output the result of the comparison:
+// 5 is less than 10.
+
+// 6. Write code to output the result of the comparison:
+// 10 is greater than 5.
+
+// 7. Write code to output the result of the comparison:
+// "Apples" is NOT equal to "Oranges"
+
+// 8. Write code to output the result of the comparison:
+// 10 * 10 is greater than 50 AND 5 * 5 is less than 50
+
+// 9. Write code to output the result of the comparison:
+// 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
+
+// 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
