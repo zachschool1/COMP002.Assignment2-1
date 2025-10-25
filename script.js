e256a357d4d7c8ff55dec3beead18f4336895523
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-10-24 21:55:50 -0700

    all code written, no commits between since it was only 1 line per question

--- script.js
+++ script.js
@@ -3,30 +3,47 @@
 
 // 1. Write code to output the following string to the console:
 // "Hello World!"
+console.log("Hello World!");
 
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
+console.log("Hello" + "World!");
 
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
 // a string?...
+console.log("Hello\nWorld!"); //\n is the new line character
 
 // 4. Write code to output the result of the arithmetic expression to the console:
 // 3 * 8 + 2;
 
+console.log(3 * 8 + 2);
+
 // 5. Write code to output the result of the comparison:
 // 5 is less than 10.
+
+console.log(5 < 10);
 
 // 6. Write code to output the result of the comparison:
 // 10 is greater than 5.
 
+console.log(10 > 5);
+
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
+
+console.log("Apples" == "Oranges");
 
 // 8. Write code to output the result of the comparison:
 // 10 * 10 is greater than 50 AND 5 * 5 is less than 50
 
+console.log(10 * 10 > 50 && 5 * 5 < 50);
+
 // 9. Write code to output the result of the comparison:
 // 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
 
+console.log (1 + 2 > 50 || 1 * 10 > 5);
+
 // 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+
+console.log("5" - 1); //automatically converts the 5 to an integer
